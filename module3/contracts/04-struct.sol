87f7a85068b1bc87f92872ec0701decee2faa92e
Alan Davis <155010643+akdavis83@users.noreply.github.com>
2026-02-06 08:22:50 -0600

    Remove 'public' visibility from Car struct fields

--- module3/contracts/04-struct.sol
+++ module3/contracts/04-struct.sol
@@ -6,10 +6,10 @@ contract Cars {
     enum CarStatus { driving, parked }
 
     struct Car {
-        bytes3 public colour;
-        uint8 public doors;
-        CarStatus public status;
-        address public owner;
+        bytes3 colour;
+        uint8 doors;
+        CarStatus status;
+        address owner;
     }
 
     constructor() {
